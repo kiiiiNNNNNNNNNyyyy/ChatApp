cfd97c37e80e7ceda2286837f4bbe5528f4d772e
Arjun  Dass <ad.sam007@gmail.com>
2017-05-16 17:46:45 -0500

    Added socket implementation

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io(); //method available due to socket  io. variable for communicat
 
 socket.on('connect', function(){
     console.log("Connected to the server!");
-
-    socket.emit("createMessage", {
-        from: 'Andrew',
-        msg: "Yup!, That was me!"
-    });
 });
 
 socket.on('disconnect', function(){
